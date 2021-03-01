2093b2d2d1fa0ac1b1243f68b815e897e27db1aa
seba-decoo <77048932+seba-decoo@users.noreply.github.com>
2021-03-01 18:37:56 +0100

    added grid view

--- preprocessing.py
+++ preprocessing.py
@@ -16,14 +16,33 @@ df1= ds[ds["target"] == 1]
 #list of attributes and remove target
 attr = list(ds.columns)
 attr.pop(-1)
-
-#loop over attrutes and plotting 2 histogram on top of each other
-
-for at in attr: 
+'''
+#loop over attributes and plotting 2 histogram on top of each other
+for at in attr:
     plt.figure()
     plt.hist(df0[at])
     plt.hist(df1[at])
     plt.show()
+    a = a+1
+'''
+#creating grid image of all attributes with 2 overlapping histograms of df0/df1
+
+plt.figure()
+
+a=0
+b=0
+
+for at in attr:
+    plt.subplot2grid((4,4), (a,b))
+    plt.hist(df0[at])
+    plt.hist(df1[at])
+    plt.grid(True)
+    b += 1
+    if b == 4:
+        b=0
+        a += 1
+plt.show()
+
 '''
 
 
--- test.py
+++ test.py
@@ -1,55 +1,19 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-from matplotlib.ticker import NullFormatter  # useful for `logit` scale
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# make up some data in the interval ]0, 1[
-y = np.random.normal(loc=0.5, scale=0.4, size=1000)
-y = y[(y > 0) & (y < 1)]
-y.sort()
-x = np.arange(len(y))
-
-# plot with various axes scales
-plt.figure()
-
-# linear
-plt.subplot(221)
-plt.plot(x, y)
-plt.yscale('linear')
-plt.title('linear')
-plt.grid(True)
+def annotate_axes(fig):
+    for i, ax in enumerate(fig.axes):
+        ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
+        ax.tick_params(labelbottom=False, labelleft=False)
 
 
-# log
-plt.subplot(222)
-plt.plot(x, y)
-plt.yscale('log')
-plt.title('log')
-plt.grid(True)
+fig = plt.figure()
+ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
+ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2)
+ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2)
+ax4 = plt.subplot2grid((3, 3), (2, 0))
+ax5 = plt.subplot2grid((3, 3), (2, 1))
 
-
-# symmetric log
-plt.subplot(223)
-plt.plot(x, y - y.mean())
-plt.yscale('symlog', linthreshy=0.01)
-plt.title('symlog')
-plt.grid(True)
-
-# logit
-plt.subplot(224)
-plt.plot(x, y)
-plt.yscale('logit')
-plt.title('logit')
-plt.grid(True)
-# Format the minor tick labels of the y-axis into empty strings with
-# `NullFormatter`, to avoid cumbering the axis with too many labels.
-plt.gca().yaxis.set_minor_formatter(NullFormatter())
-# Adjust the subplot layout, because the logit one may take more space
-# than usual, due to y-tick labels like "1 - 10^{-3}"
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                    wspace=0.35)
+annotate_axes(fig)
 
 plt.show()
