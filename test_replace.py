1cb554535f7af9d6c719e3d89c4765c1abd262cc
seba-decoo <77048932+seba-decoo@users.noreply.github.com>
2021-03-02 22:29:14 +0100

    classification_test script with adjusted depth and encoded catogorial values

--- test_replace.py
+++ test_replace.py
@@ -1,5 +1,6 @@
 #test replace function on dataset
 
+
 from os import sep
 import pandas
 
@@ -21,11 +22,83 @@ print(ds)
 
 ds.to_csv("heart_preprocessed.csv", sep=",", index=False)
 
+##construct classification tree
+#define X and y value
+X = ds.drop('target', axis=1)
 
+#encode categorial values
+
+from category_encoders import OrdinalEncoder
+
+# Categorical boolean mask
+categorical_feature_mask = ds.dtypes==object
+# filter categorical columns using mask and turn it into a list
+categorical_cols = ds.columns[categorical_feature_mask].tolist()
+
+X= OrdinalEncoder(cols=categorical_cols).fit_transform(ds)
+
+#print(X)
+
+
+y = ds['target']
+#split data in training and test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, \
+    test_size=0.33) # specify size of training set
+
+#build up decision tree
+
+#plt.figure(figsize=[14,12])     #uncomment for direct visualisation
+decision_tree = DecisionTreeClassifier(random_state=0)
+decision_tree = decision_tree.fit(X_train, y_train)
+tree.plot_tree(decision_tree)
+#plt.show()     #uncomment for direct visualisation
+
+#visualize with graphviz 
+#!!! add to path of OS (explanation --> see begin of script)!!!
+
+import graphviz
+dot_data = tree.export_graphviz(decision_tree, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render("dicision_tree")
+
+#visualize text_based tree
+attr_names = list(ds.columns)
+attr_names.pop(-1)
+r = export_text(decision_tree, attr_names)
+print(r)
+
+#evaluation
+y_pred = decision_tree.predict(X_test)
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+'''
+##################################################################
 #encode categorie attributes
 #link to expanation
 #https://scikit-learn.org/stable/modules/preprocessing.html (6.3.4)
+#https://towardsdatascience.com/encoding-categorical-features-21a2651a065c
+
+# Categorical boolean mask
+categorical_feature_mask = ds.dtypes==object
+# filter categorical columns using mask and turn it into a list
+categorical_cols = ds.columns[categorical_feature_mask].tolist()
+categorical_features = ds.select_dtypes("object").columns
+
+print(categorical_feature_mask)
+print(categorical_cols)
 
 
 
+# import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+# instantiate OneHotEncoder
+ohe = OneHotEncoder(categories=categorical_feature_mask, sparse=False ) 
+# categorical_features = boolean mask for categorical columns
+# sparse = False output an array not sparse matrix
+# apply OneHotEncoder on categorical feature columns
+X_ohe = ohe.fit_transform(ds) # It returns an numpy array
+'''
 
