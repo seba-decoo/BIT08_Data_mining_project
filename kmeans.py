bb3d377ebe416cd563a9e9c18629cc3de60debe6
evhauwae <emielvanhauwaert1@gmail.com>
2021-03-08 15:43:18 +0100

    weka-like visualization of kmeans clustering

--- kmeans.py
+++ kmeans.py
@@ -16,13 +16,23 @@ dist = kmeans.transform(dfnt)
 # script to go over all attributes and plot them against each other. try to do this for kmeans. can be compared to weka
 print(dfnt)
 
+a = 0
+b = 0
+
+plt.figure(figsize=[200,200])
 for att in dfnt:
     for a2 in dfnt:
-        plt.scatter(dfnt[att],dfnt[a2],c=df["target"], cmap="prism")
-        plt.xlabel(att)
-        plt.ylabel(a2)
-        plt.show()
-
+        plt.subplot2grid((14,14),(a,b))
+        plt.scatter(dfnt[att],dfnt[a2],c=y, cmap="prism", s=5,marker=".")
+        plt.xlabel(att) # kan mss weg indien niet te vinden hoe size kan verbeterd worden
+        plt.ylabel(a2) # zie boven
+        #plt.show()
+        b += 1
+        if b == 13:
+            b = 0
+            a += 1
+plt.tight_layout()
+plt.show()
 
 """
 plt.scatter(dist[:,[0]],dist[:,[1]], c=y, cmap="PRGn")
